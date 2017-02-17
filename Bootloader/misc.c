42a78cee46ed67bb14b076aab8d244f3c32a17db
ufuk onder <ufuk.onder@gmail.com>
2017-02-17 23:19:26 +0300

    utility functions
    ascii and decimal operation functions, for ascii coded decimal
    communication

---
+++ Bootloader/misc.c
@@ -0,0 +1,26 @@
+/*! \file misc.c
+    \brief   miscellaneous functions
+	
+	\copyright Copyright (c) 2017 u-onder
+*/
+
+#include "misc.h"
+
+char_t IntToHex(int_t x)
+{
+  return (x > 9 ? 'A' + x - 10 : '0' + x);
+}
+
+int_t HexToInt(char_t x)
+{
+  if ((x >= '0') && (x <= '9'))
+    return x - '0';
+  
+  if ((x >= 'a') && (x <= 'f'))
+    return x - 'a' + 10;
+  
+  if ((x >= 'A') && (x <= 'F'))
+    return x - 'A' + 10;
+  
+  return 0;
+}
---
+++ Bootloader/misc.h
@@ -0,0 +1,9 @@
+#ifndef _MISC_H_
+#define _MISC_H_
+
+#include "cpu.h"
+
+char_t  IntToHex      (int_t x);
+int_t   HexToInt      (char_t x);
+
+#endif
