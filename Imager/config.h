a07d26752d955fbd4c87e977b930810fef94a4bd
ufuk onder <ufuk.onder@gmail.com>
2017-02-19 09:38:47 +0300

    Configuration

---
+++ Imager/config.h
@@ -0,0 +1,17 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define FlashSize_d				(1024*1024)
+
+#define FlashAddressOfset_d		(0x08000000)
+
+#define FlashBootAddress_d		(0x08004000)
+#define FlashSignatureAddress_d	(0x080FFFE0)
+#define FlashHashAddress_d		(0x080FFFF0)
+#define FlashEndAddress_d		(0x080FFFFF)
+
+#define MaximumBlockSize_d		(64)
+
+#define CIPHER_ROUNDS	32
+
+#endif
